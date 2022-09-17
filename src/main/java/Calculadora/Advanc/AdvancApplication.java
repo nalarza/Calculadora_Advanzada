f35546f8c5b4941b00cb6665b0bc8a675c0c4c9d
Nelson Andres Alarza Andrade <nalarzaandrade@gmail.com>
2022-09-17 11:40:34 -0500

    commit inicial con java 17 y gradel

---
+++ src/main/java/Calculadora/Advanc/AdvancApplication.java
@@ -0,0 +1,13 @@
+package Calculadora.Advanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Calculadora/Advanc/AdvancApplicationTests.java
@@ -0,0 +1,13 @@
+package Calculadora.Advanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
